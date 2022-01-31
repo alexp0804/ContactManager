32595fe2925ed57ab1353d00e3c0ed40bf00827a
Alex Peterson <alexpeterson0804@gmail.com>
2022-01-31 17:41:14 -0500

    Search endpoint

---
+++ api/SearchContacts.php
@@ -0,0 +1,74 @@
+<?php
+
+    $inData = getRequestInfo();
+
+	$searchResults = "";
+	$searchCount = 0;
+
+    // Connect to Contacts database
+    $serverName = "localhost";
+    $dBUsername = "root";
+    $dBPassword = "Group1Team";
+    $dbName = "COP4331";
+
+    $conn = new mysqli($serverName, $dBUsername, $dBPassword, $dbName);
+	if ($conn->connect_error) 
+	{
+		returnWithError($conn->connect_error);
+	} 
+	else
+	{
+		$stmt = $conn->prepare("select FIRSTNAME from Contacts where FIRSTNAME like ? and UserID=?");
+		$colorName = "%" . $inData["search"] . "%";
+		$stmt->bind_param("ss", $colorName, $inData["userID"]);
+		$stmt->execute();
+		
+		$result = $stmt->get_result();
+		
+		while ($row = $result->fetch_assoc())
+		{
+			if ($searchCount > 0)
+			{
+				$searchResults .= ",";
+			}
+			$searchCount++;
+			$searchResults .= '"' . $row["Name"] . '"';
+		}
+		
+		if ($searchCount == 0)
+		{
+			returnWithError("No Records Found");
+		}
+		else
+		{
+			returnWithInfo($searchResults);
+		}
+		
+		$stmt->close();
+		$conn->close();
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithInfo( $searchResults )
+	{
+		$retValue = '{"results":[' . $searchResults . '],"error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
