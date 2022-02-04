a8a7c01360ce17751dbb88feed46c8b8e0c08eb1
Leo5620LG <97623224+Leo5620LG@users.noreply.github.com>
2022-02-03 20:53:02 -0500

    reverting

--- js/code.js
+++ js/code.js
@@ -419,9 +419,6 @@ function doSearch(search)
 
                     // Fill table with contacts
                     var contacts = jsonObject.results;
-			contacts.sort(function (a, b) {
-  			return a.localeCompare(b); 
-			});
                     var info_fields = ["FIRSTNAME", "LASTNAME", "EMAIL", "PHONENUMBER", "ID", "USERID"];
 
                     // For each of the contacts
