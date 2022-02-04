fe91fbc16f6aec2f9fc938e4260b4ab28b8e48e7
Alex Peterson <alexpeterson0804@gmail.com>
2022-02-03 19:15:37 -0500

    Revert "remove sort"
    This reverts commit 65f47aa42f8ef85ddc657f6d2f56aa03638f58aa.

--- js/code.js
+++ js/code.js
@@ -391,6 +391,21 @@ function callSearch()
     doSearch(document.getElementById("SearchInput").value);
 }
 
+function compare(a, b)
+{
+    if (a[FIRSTNAME] < b[FIRSTNAME])
+    {
+        return -1;
+    }
+    else if (a[FIRSTNAME] > b[FIRSTNAME])
+    {
+        return 1;
+    }
+
+    return 0;
+}
+
+
 function doSearch(search)
 {
     userId = readCookie();
@@ -419,6 +434,8 @@ function doSearch(search)
                 {
                     // Fill table with contacts
                     var contacts = jsonObject.results;
+                    contacts.sort(compare);
+
                     var info_fields = ["FIRSTNAME", "LASTNAME", "EMAIL", "PHONENUMBER", "ID", "USERID"];
 
                     // For each of the contacts
