758236d6141a6d82e34bb2be9316b4870e539bdf
Leo5620LG <97623224+Leo5620LG@users.noreply.github.com>
2022-02-03 21:13:17 -0500

    Update code.js

--- js/code.js
+++ js/code.js
@@ -419,6 +419,7 @@ function doSearch(search)
 
                     // Fill table with contacts
                     var contacts = jsonObject.results;
+			contacts.sort(function(a, b){return a-b});
                     var info_fields = ["FIRSTNAME", "LASTNAME", "EMAIL", "PHONENUMBER", "ID", "USERID"];
 
                     // For each of the contacts
